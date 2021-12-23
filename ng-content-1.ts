222917f162526d9cb0ce80423ef17dc23dfa6ef7
docaohuynh <docaohuynh0601@gmail.com>
2021-12-23 08:28:41 +0700

    Create ng-content-1.ts

---
+++ ng-content-1.ts
@@ -0,0 +1,17 @@
+
+@Component({
+  selector: 'app-root',
+  template: `
+
+    <h1>FA Input</h1>
+
+    <fa-input icon="envelope" (value)="onNewValue($event)"></fa-input>
+    
+    <fa-input icon="lock" (value)="onNewValue($event)"></fa-input>
+  `
+})
+export class AppComponent {
+  onNewValue(val) {
+    console.log(val);
+  }
+}
